d02a33ebc34db87b125f4b1d0cbd62e8430bb816
aokijun <aokijun@aokijun-no-MacBook-Air.local>
2018-10-21 01:28:38 +0900

    fix again

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,9 @@ module BookShelf
     config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
+	config.assets.initialize_on_precompile = false
+   
+ # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
   end
